de67ec494ea9d739d457b15f34d8326109b86897
sravya-accolite <52782916+sravya-accolite@users.noreply.github.com>
2019-07-29 09:25:06 +0530

    Delete SQL9.sql

--- 16july_PM/HR-SCHEMA/SQL9.sql
+++
@@ -1,9 +0,0 @@
-
-  
-        
-delimiter $$
-create procedure updateSal(IN empId int)
-BEGIN
-	call getNewSalary(empId,13,@new_sal);
-	update employee set salary=@new_sal where employee_id=empId;
-END$$
