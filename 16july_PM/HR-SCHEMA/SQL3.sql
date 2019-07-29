713fa6e4137de4b0194b25a6fc1c8a48b1059971
sravya-accolite <52782916+sravya-accolite@users.noreply.github.com>
2019-07-29 11:02:49 +0530

    Delete SQL3.sql

--- 16july_PM/HR-SCHEMA/SQL3.sql
+++
@@ -1,7 +0,0 @@
-select l.city,d.department_name,count(e.employee_id)
-from locations l 
-left join departments d 
-on l.location_id=d.location_id
-left join employees e 
-on d.department_id=e.department_id
-group by l.city,d.department_name;
