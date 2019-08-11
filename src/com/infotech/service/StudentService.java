569760f26252a63d503f78aadc5ff1b6fa222786
sravya-accolite <52782916+sravya-accolite@users.noreply.github.com>
2019-08-11 07:43:27 +0530

    Delete StudentService.java

--- src/com/infotech/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.infotech.service;
-
-import com.infotech.model.Student;
-
-public interface StudentService {
-	public abstract Student validateStudentCredential(String email,	String password);
-	public abstract boolean registerStudent(Student student);
-
-}
